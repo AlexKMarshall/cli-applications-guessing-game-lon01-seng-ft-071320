200319acaf77ec44fbeb3f21ed897aed9fbb0e45
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-26 11:59:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,9 @@ def run_guessing_game
   computer_guess = guess
   input = get_input
   
-  puts "Goodbye!" if input === "exit"
-  puts "Sorry! The computer guessed #{computer_guess}." if input != computer_guess
+  puts respond(computer_guess, input)
+  #puts "Goodbye!" if input === "exit"
+  #puts "Sorry! The computer guessed #{computer_guess}." if input != computer_guess
 end
 
 def get_input
